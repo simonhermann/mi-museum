51f8fd1e9e4e94ebe27ae6ed81adffcf741e08ad
simonsilabs <hermann@si-labs.com>
2018-04-22 23:12:34 +0200

    nice error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,28 +1,47 @@
 <template>
-  <div>
+  <div class="error-wrapper">
+    <h1>~(◎_◎)~</h1>
     <div v-if="error.statusCode === 404">
-      <h4>Seite nicht gefunden, bitte kehre <br>
-        <nuxt-link to="/">zurück zur Startseite</nuxt-link>
-      </h4>
+      <p class="error-msg">Seite nicht gefunden. <br>
+        <nuxt-link to="/">Hier geht's zurück zur Startseite</nuxt-link>
+      </p>
     </div>
     <div v-else>
-      <h4>Etwas ist schiefgelaufen, vermutlich weil ein Update eingespielt wurde (Yay!).
-        Bitte versuche, die Seite neu zu laden. oder kehre <br>
-        <nuxt-link to="/">zurück zur Startseite</nuxt-link>
-      </h4>
+      <p class="error-msg">Etwas ist schiefgelaufen, vermutlich weil ein Update eingespielt wurde (Yay!).
+        Bitte versuche, <br> <a href="javascript:location.reload()">die Seite neu zu laden</a>.
+      </p>
     </div>
+    <p class="error-output">
+      Falls du mich siehst, zeig mir diese Nachricht: <br>
+      <code>{{ error }}</code>
+    </p>
   </div>
 </template>
 
 <script>
 export default {
   props: ['error'],
+  beforeMount() {
+    document.documentElement.style.setProperty('--color-highlight', 'darkred')
+  }
 }
 </script>
 
-<style scoped>
-h4 {
-  text-decoration: none;
+<style lang="scss" scoped>
+.error-wrapper {
+  max-width: 20rem;
+  margin: auto;
   text-align: center;
+}
+.error-msg {
+  margin-bottom: 10vh;
+  font-size: 1.5rem;
+}
+.error-output {
+  color: #999;
+  font-size: .75rem;
+  code {
+    white-space: normal;
+  }
 }
 </style>
