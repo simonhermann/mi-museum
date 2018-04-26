65cd089cb166a112aed0b608667f4b4a482a1a23
simonsilabs <hermann@si-labs.com>
2018-04-26 13:58:39 +0200

    refactor menu label

--- components/navdrawer.vue
+++ components/navdrawer.vue
@@ -1,13 +1,12 @@
 <template>
   <div>
     <v-navigation-drawer v-model="drawer" fixed app floating :touchless="$vuetify.breakpoint.mdAndUp" width="300" mobile-break-point="1264">
-     
-      <button type="button" class="btn btn--icon" @click="drawer = !drawer" style="margin-left:-1em" aria-label="Close" tabindex="0">
+
+      <a class="btn btn--icon" @click="drawer = !drawer" style="margin-left:-1em" aria-label="Close" tabindex="0">
         <div class="btn__content">
           <i aria-hidden="true" class="icon__chevron-left">‹</i>
         </div>
-      </button>
-
+      </a>
 
       <header>
         <nuxt-link class="nav-homelink" to="/">
@@ -40,19 +39,19 @@
 
     </v-navigation-drawer>
 
-    <v-toolbar fixed :scroll-off-screen="!$vuetify.breakpoint.mdAndUp" app dense 
-    :scroll-threshold="50" :floating="$vuetify.breakpoint.mdAndUp">
+    <v-toolbar fixed :scroll-off-screen="!$vuetify.breakpoint.mdAndUp" app dense :scroll-threshold="50" :floating="$vuetify.breakpoint.mdAndUp">
 
-      <button type="button" class="toolbar__side-icon btn btn--icon btn__menu" :class="{ transparent : drawer }" @click="drawer = !drawer" style="position: relative;" aria-label="Menu" tabindex="0">
+      <a class="toolbar__side-icon btn btn--icon btn-menu" :class="{ transparent : drawer }" @click="drawer = !drawer" style="position: relative;" aria-label="Menu" tabindex="0">
         <div class="btn__content">
-          <i class="icon__menu">
-            <svg fill="#424242" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-                <path d="M0 0h24v24H0z" fill="none"/>
-                <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/>
+          <i class="btn-menu__icon">
+            <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+              <path d="M0 0h24v24H0z" fill="none" />
+              <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
             </svg>
           </i>
+          <span class="btn-menu__label">Menü</span>
         </div>
-      </button>
+      </a>
 
       <div class="toplink" v-on:click="scrollToTop"></div>
 
@@ -78,7 +77,6 @@
   -ms-overflow-style: -ms-autohiding-scrollbar;
 
   &:not(&--is-mobile) {
-    //overflow: -moz-scrollbars-none;
     -ms-overflow-style: none;
     &::-webkit-scrollbar {
       display: none;
@@ -94,7 +92,8 @@
     font-size: 3em;
     line-height: 3em;
     display: block;
-    margin-top: -0.27em;
+    margin-top: -0.23em;
+    margin-left: -0.01em;
   }
   .nav-homelink {
     text-decoration: none;
@@ -123,7 +122,7 @@
     &::before {
       content: '›';
       color: transparent;
-      transition: margin 200ms var(--transition-curve1);
+      transition: transform 200ms var(--transition-curve1);
       text-decoration: none !important;
       position: absolute;
       font-size: 1.5em;
@@ -134,7 +133,7 @@
       color: var(--the-color);
       &::before {
         color: inherit;
-        margin-left: -0.85em;
+        transform: translateX(-0.125rem);
       }
     }
   }
@@ -163,7 +162,7 @@
 .toolbar {
   //background-color: rgba(245, 245, 245, 1);
   background-color: transparent !important; //vuetify hack
-  padding-top: .5rem !important; //vuetify hack
+  padding-top: 0.5rem !important; //vuetify hack
   box-shadow: none !important;
   @include mq-md {
     padding-top: 1rem !important; //vuetify hack
@@ -191,29 +190,39 @@
     background-color: inherit;
     justify-content: space-between;
 
-    .btn__menu {
+    .btn-menu {
       margin-left: 1rem !important; //vuetify hack
       transition: opacity 150ms ease-out;
       &.transparent {
         opacity: 0;
       }
-      .icon__menu {
+      &__icon {
         color: inherit;
         height: 2em;
         width: 2em;
         margin-top: 0.2em;
-
+        fill: var(--color-text);
+      }
+      &__label {
+        color: var(--color-text);
+        font-style: italic;
+        font-size: 1rem;
+        position: absolute;
+        margin-top: -0.08em;
+        margin-left: 2em;
+        text-transform: none;
+        transition: transform 200ms var(--transition-curve1);
+        display: none;
         @include mq-sm {
-          &::after {
-            content: 'Menü';
-            color: inherit;
-            position: absolute;
-            font-size: 1.25em;
-            margin-top: -0.18em;
-            margin-left: 0.4em;
-            display: inline-block;
-            text-transform: none;
-          }
+          display: inline-block;
+        }
+      }
+      &:hover {
+        .btn-menu__icon {
+          fill: var(--the-color);
+        }
+        .btn-menu__label {
+          transform: translateX(0.15rem);
         }
       }
     }
@@ -256,6 +265,13 @@
       display: none;
     }
   }
+  .btn__content:before {
+    margin-left: -0.1rem;
+    margin-top: -0.0325rem;
+  }
+}
+.btn__content:before {
+  color: var(--the-color);
 }
 </style>
 
